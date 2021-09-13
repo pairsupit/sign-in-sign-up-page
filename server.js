e1a17e17223104aa1d97e07b1355c7749e933b4e
pairsupit <53257721+pairsupit@users.noreply.github.com>
2021-09-13 14:16:46 +0700

    create frontend manage page

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
-app.use(express.json())
+var admin = require('firebase-admin');
+admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    databaseURL: 'https://<DATABASE_NAME>.firebaseio.com'
+});
 
-app.get('/homepage',(req,res) => {
-    res.render('homepage');
+app.get('/', function (req, res) {
+    res.send('hello world')
 })
-
 
 app.listen(`8080`,()=>{
     console.log('port is running on 8080');
